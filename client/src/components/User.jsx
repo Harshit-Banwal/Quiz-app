5730ea70734d59459a5646a85f472b269cf36f28
Harshit-Banwal <harshitbanwal849@gmail.com>
2024-07-08 20:32:58 +0530

    user panel changes

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,13 +1,22 @@
 // UserPanel.js
-import "../assets/css/Home.css";
-import React, { useState } from "react";
+import '../assets/css/Home.css';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const User = () => {
   // Sample user data
   return (
-    <div className="margin-top">
-      <h1>User Panel</h1>
-    </div>
+    <Container className="margin-top text-center">
+      <h2>Welcome to the User Panel</h2>
+      <Row className="mt-4">
+        <Col>
+          <Link to="/quizzes" className="btn btn-primary space">
+            View Quizzes
+          </Link>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
